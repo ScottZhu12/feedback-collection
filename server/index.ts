72e63398d78a3c34215aefe601ba5cae48a9d72a
Scott Zhu <scott391113895@gmail.com>
2022-06-24 13:32:55 -0700

    server updated

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,8 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
+// create express application and define port number
 const app: Express = express();
-const port = process.env.SERVER_PORT || 8000;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req: Request, res: Response) => {
   try {

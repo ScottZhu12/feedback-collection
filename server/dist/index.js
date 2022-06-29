1b1226daf02bb2cc7cb7f078636f066e3bd0a6e5
ScottZhu12 <scott391113895@gmail.com>
2022-06-28 21:52:40 -0800

    google authentication setup added

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,22 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// create express application and define port number
+const passport_1 = __importDefault(require("passport"));
+const passport_google_oauth20_1 = __importDefault(require("passport-google-oauth20"));
+const keys_1 = require("./config/keys");
+const keys_2 = require("./config/keys");
+// create express application (server) and define port number
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
-app.get('/', (req, res) => {
-    try {
-        res.status(200).json({
-            status: 'success',
-            data: {
-                message: 'Welcome',
-            },
-        });
-    }
-    catch (err) {
-        console.log(err);
-    }
-});
+const port = process.env.PORT || 5000;
+// configure google strategy
+const GoogleStrategy = passport_google_oauth20_1.default.Strategy;
+passport_1.default.use(new GoogleStrategy({
+    clientID: keys_1.googleClientID,
+    clientSecret: keys_2.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    passReqToCallback: true,
+}, (accessToken) => {
+    console.log(accessToken);
+}));
+// setup google oauth route
+app.get('/auth/google', passport_1.default.authenticate('google', {
+    scope: ['email', 'profile'],
+}));
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,43 @@
 import express, { Express, Request, Response } from 'express';
 import passport from 'passport';
-import GoogleStrategy from 'passport-google-oauth';
+import passportGoogle from 'passport-google-oauth20';
 
-// create express application and define port number
+import { googleClientID } from './config/keys';
+import { googleClientSecret } from './config/keys';
+
+// create express application (server) and define port number
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req: Request, res: Response) => {
-  try {
-    res.status(200).json({
-      status: 'success',
-      data: {
-        message: 'Welcome',
-      },
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+// configure google strategy
+// callback function will be executed when user autherized app to access
+// their google account to login (when hits the google oauth callback route)
+const GoogleStrategy = passportGoogle.Strategy;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      passReqToCallback: true,
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// setup google oauth route
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile'],
+  })
+);
+
+// setup google oauth callback route
+// in the callback route url, the query code is sent back from google
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(port, () => {
   console.log(`server is running at http://localhost:${port}`);
